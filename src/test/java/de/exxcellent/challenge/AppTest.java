7bdb0a446d66e3be82fd6f3c60093702639431b8
Rchn1907 <ahmetruchan.arican@gmail.com>
2025-03-09 00:02:51 +0100

    Seperated my App tests and module test for weather Program especially

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,62 +1,37 @@
 package de.exxcellent.challenge;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 /**
  * Example JUnit 5 test case.
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
 
-    private static final String TEST_FILE = "test_weather.csv";    
-
-    @BeforeEach
-    void setUp() throws IOException {
-        // Creating mock data for testing the Methods of my Program
-        try(FileWriter writer = new FileWriter(TEST_FILE)) {
-            writer.write("Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n");
-            writer.write("1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n");
-            writer.write("2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5\n");
-            writer.write("3,77,55,66,39.6,0,350,5,350,9,2.8,59,24,1016.8\n");
-            writer.write("4,77,59,68,51.1,0,110,9.1,130,12,8.6,62,40,1021.1\n");
-        }
-    }
-
-    @Test
-    void testFindDayWithSmallestTemperatureSpread() {
-        int result = WeatherAnalyzer.findDayWithSmallestTempSpread(TEST_FILE);
-        assertEquals(2, result, "The day with the smallest temperature should be correct!");
-    }
-
-    @Test
-    void testEmptyFile() throws IOException {
-        Files.write(Path.of(TEST_FILE), "".getBytes());
-        int result = WeatherAnalyzer.findDayWithSmallestTempSpread(TEST_FILE);
-        assertEquals(-1, result, "Should return -1 for empty file!");
-    }
-
-    @Test
-    void testIncorrectFormat() throws IOException{
-        try(FileWriter writer = new FileWriter(TEST_FILE)) {
-            writer.write("Wrong,Format\n");
-        }
-        int result = WeatherAnalyzer.findDayWithSmallestTempSpread(TEST_FILE);
-        assertEquals(-1, result, "Should return -1 for incorrect file format!");
-    }
-
     @Test
     void runFootball() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
         App.main("--football", "football.csv");
+
+        String output = outputStream.toString().trim();
+        assertTrue(output.contains("Team with smallest goal spread")); // Check if normal program is running with the right arguments
+    }
+
+    @Test
+    void runWeather() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        App.main("--weather", "weather.csv");
+
+        String output = outputStream.toString().trim();
+        assertTrue(output.contains("Day with smallest temperature spread")); // Check if normal program is running with the right arguments
     }
 
 }
---
+++ src/test/java/de/exxcellent/challenge/WeatherTest.java
@@ -0,0 +1,50 @@
+package de.exxcellent.challenge;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WeatherTest {
+    private static final String TEST_FILE = "src/main/resources/de/exxcellent/challenge/test_weather.csv";   
+    private WeatherAnalyzer analyzer = new WeatherAnalyzer(); 
+
+    @BeforeEach
+    void setUp() throws IOException {
+        // Creating mock data for testing the Methods of my Program
+        try(FileWriter writer = new FileWriter(TEST_FILE)) {
+            writer.write("Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n");
+            writer.write("1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n");
+            writer.write("2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5\n");
+            writer.write("3,77,55,66,39.6,0,350,5,350,9,2.8,59,24,1016.8\n");
+            writer.write("4,77,59,68,51.1,0,110,9.1,130,12,8.6,62,40,1021.1\n");
+        }
+    }
+
+    @Test
+    void testFindDayWithSmallestTemperatureSpread() {
+        String result = analyzer.analyze(TEST_FILE);
+        assertEquals("2", result, "The day with the smallest temperature should be correct!");
+    }
+
+    @Test
+    void testEmptyFile() throws IOException {
+        Files.write(Path.of(TEST_FILE), "".getBytes());
+        int result = analyzer.analyze(TEST_FILE);
+        assertEquals(-1, result, "Should return -1 for empty file!");
+    }
+
+    @Test
+    void testIncorrectFormat() throws IOException{
+        try(FileWriter writer = new FileWriter(TEST_FILE)) {
+            writer.write("Wrong,Format\n");
+        }
+        int result = analyzer.analyze(TEST_FILE);
+        assertEquals(-1, result, "Should return -1 for incorrect file format!");
+    }
+}
