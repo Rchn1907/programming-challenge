7931b4fbf3f97946ec8c949ea8bd5394a39cc9a7
Rchn1907 <ahmetruchan.arican@gmail.com>
2025-03-09 00:03:16 +0100

    Added FootballAnalyzer Test

---
+++ src/test/java/de/exxcellent/challenge/FootballTest.java
@@ -0,0 +1,51 @@
+package de.exxcellent.challenge;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FootballTest {
+    private static final String TEST_FILE = "src/main/resources/de/exxcellent/challenge/football.csv";   
+    private FootballAnalyzer analzyer = new FootballAnalyzer();
+
+    @BeforeEach
+    void setUp() throws IOException {
+        // Creating mock data for testing the Methods of my Program
+        try(FileWriter writer = new FileWriter(TEST_FILE)) {
+            writer.write("Team,Games,Wins,Losses,Draws,Goals,Goals Allowed,Points\n");
+            writer.write("Arsenal,38,26,9,3,79,36,87\n");
+            writer.write("Liverpool,38,24,8,6,67,30,80\n");
+            writer.write("Manchester United,38,24,5,9,87,45,77\n");
+            writer.write("Newcastle,38,21,8,9,74,52,71\n");
+        }
+    }
+
+    @Test
+    void testFindDayWithSmallestTemperatureSpread() {
+        String result = analyzer.analyze(TEST_FILE);
+        assertEquals("Newcastle", result, "Expected team with smallest goal difference is Newcastle!");
+    }
+
+    @Test
+    void testEmptyFile() throws IOException {
+        Files.write(Path.of(TEST_FILE), "".getBytes());
+        int result = analyzer.analyze(TEST_FILE);
+        assertEquals(-1, result, "Should return -1 for empty file!");
+    }
+
+    @Test
+    void testIncorrectFormat() throws IOException{
+        try(FileWriter writer = new FileWriter(TEST_FILE)) {
+            writer.write("Wrong,Format\n");
+        }
+        int result = analyzer.analyze(TEST_FILE);
+        assertEquals(-1, result, "Should return -1 for incorrect file format!");
+    }
+
+}
